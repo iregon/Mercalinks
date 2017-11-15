53f59d4b6feee022443fcc765ebb709a5e4c0f3e
iregon <0iregon0@gmail.com>
2017-11-15 23:00:21 +0100

    Login http request, bugfix

--- www/templates/login/controller.js
+++ www/templates/login/controller.js
@@ -1,5 +1,48 @@
-angular.module('starter.controllers')
+angular.module('starter')
 
-.controller('LoginCtrl', function($scope, $http) {
+.controller('LoginCtrl', function($scope, $http, $timeout, $ionicModal) {
 
+  $ionicModal.fromTemplateUrl('templates/login/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    // console.log('Doing login', $scope.loginData);
+
+    var link = "http://mercalinks.altervista.org/login.php";
+    $http.get(link, {
+      params: {
+        str: $scope.loginData.email
+      }
+    }).then(function(response) {
+      $scope.annunci = response.data;
+      console.log($scope.annunci);
+    }).catch(function(error) {
+      console.log(error);
+    });
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    // $timeout(function() {
+    //   var link = "http://mercalinks.altervista.org/login.php";
+    //   $http.post(link, {
+    //     params: {
+    //       str: $scope.loginData.email + ";" + $scope.loginData.password
+    //     }
+    //   }).then(function(response) {
+    //     $scope.annunci = response.data;
+    //     console.log($scope.annunci);
+    //   }).catch(function(error) {
+    //     console.log(error);
+    //   });
+    //   // $scope.closeLogin();
+    // }, 3000);
+  };
 });
