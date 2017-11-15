2656162c21ce7f1ad0de2fcbf49ca775a15d1f60
iregon <0iregon0@gmail.com>
2017-11-15 18:18:04 +0100

    Creazione della pagina login

--- www/js/app.js
+++ www/js/app.js
@@ -41,6 +41,16 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   })
 
+  .state('app.login', {
+    url: '/login',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/login/login.html',
+        controller: 'LoginCtrl'
+      }
+    }
+  })
+
   .state('app.browse', {
       url: '/browse',
       views: {
