1a574e5d92677292acd1c0fdf8c5d328053c2e1b
giuliafor <giuliafortunatogf99@gmail.com>
2017-11-16 12:21:58 +0100

    annuncio dettaglio

--- www/templates/annuncio/controller.js
+++ www/templates/annuncio/controller.js
@@ -1,7 +1,20 @@
 angular.module('starter')
 
-.controller('AnnuncioCtrl', function($scope, $stateParams) {
-  var link = "http://mercalinks.altervista.org/select.php";
-  
+.controller('AnnuncioCtrl', function($scope, $stateParams, $http) {
 
+  var link = "http://mercalinks.altervista.org/select.php";
+  var id=$stateParams.annuncioId;
+  //console.log(id);
+  $http.get(link,{
+    params:{
+      id: id
+    }
+  }).then(
+    function(response){
+      $scope.annuncio=response.data.annuncio;
+      console.log(response.data.annuncio);
+    }
+  ).catch(function(error){
+    console.log(error);
+  });
 });
