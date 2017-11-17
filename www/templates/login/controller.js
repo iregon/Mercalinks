edf6c35b5ec3a6ec7a609d65cb9d93d24a3db89c
iregon <0iregon0@gmail.com>
2017-11-17 13:53:51 +0100

    Login bugfix

--- www/templates/login/controller.js
+++ www/templates/login/controller.js
@@ -23,6 +23,7 @@ angular.module('starter')
       $scope.res = response.data;
       console.log($scope.res);
       $localStorage.id_utente = $scope.res;
+      header("Location: /app/annunciUtente");
     }).catch(function(error) {
       console.log(error);
     });
