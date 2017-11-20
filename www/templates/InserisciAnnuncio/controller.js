fbbc9c53bb4bc056ef186b76a4c6437843257676
spriculo <francescorocca9@gmail.com>
2017-11-20 13:36:24 +0100

    prova controllo inserimento annuncio

--- www/templates/InserisciAnnuncio/controller.js
+++ www/templates/InserisciAnnuncio/controller.js
@@ -1,10 +1,91 @@
  angular.module('starter')
 
- .controller('inserisciAnnuncioCtrl',function($scope,$http){
+ .controller('inserisciAnnuncioCtrl',function($scope,$http,$ionicPopup){
+   // $scope.controllo=function(){
+   //   if(insert.titolo.value===null){
+   //     var alertPopup=$ionicPopup.show({
+   //       title:'Inserisci il titolo'
+   //       buttons:[{
+   //         text:'OK',
+   //         type: 'button-default'
+   //
+   //   }]
+   // });
+   //   alertPopup.then(function(res){
+   //     console.log(res);
+   //
+   //     });
+   //   }else{
+   //     if(insert.descrizione.value===null){
+   //       var alertPopup=$ionicPopup.show({
+   //         title:'Inserisci la descrizione'
+   //         buttons:[{
+   //           text:'OK',
+   //           type: 'button-default'
+   //
+   //     }]
+   //       });
+   //       alertPopup.then(function(res){
+   //         console.log(res);
+   //       });
+   //     }
+   //     else{
+   //       if(insert.prezzo.value===null){
+   //         var alertPopup=$ionicPopup.show({
+   //           title:'Inserisci il prezzo'
+   //           buttons:[{
+   //             text:'OK',
+   //             type: 'button-default'
+   //
+   //       }]
+   //         });
+   //         alertPopup.then(function(res){
+   //           console.log(res);
+   //         });
+   //       }
+   //       else{
+   //         if(insert.immagine.value===null){
+   //           var alertPopup=$ionicPopup.show({
+   //             title:'Inserisci immagine'
+   //             buttons:[{
+   //               text:'OK',
+   //               type: 'button-default'
+   //
+   //         }]
+   //           });
+   //           alertPopup.then(function(res){
+   //             console.log(res);
+   //           });
+   //         }
+   //         else{
+   //           // $scope.messaggio();
+   //         }
+   //       }
+   //     }
+   //   }
+   // }
+
+
+
+
+   $scope.messaggio=function(){
+     var alertPopup=$ionicPopup.show({
+       title:'<img src="/img/tick.png">',
+       subTitle:'<h4>Annuncio inserito</h4>',
+       buttons:[{
+         text:'OK',
+         type: 'button-default'
+
+   }]
+     });
+     alertPopup.then(function(res){
+       console.log(res);
+     });
+   };
 
    $scope.inserisci=function(){
      var link= "http://mercalinks.altervista.org/insert1.php";
-     
+
      $http.get(link, {
        params: {
          tabella:"annunci",
@@ -19,8 +100,8 @@
 
        }
      }).then(function(response){
-       $scope.res = response.data;
-       console.log($scope.res);
+
      });
+
    };
  });
