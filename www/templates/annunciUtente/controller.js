098f0bbafa155f8436d28587b261bc0deda6c1ff
AndreaF <and.felline@gmail.com>
2017-11-20 10:37:31 +0100

    aggiunte immagini annunci

--- www/templates/annunciUtente/controller.js
+++ www/templates/annunciUtente/controller.js
@@ -3,17 +3,11 @@ angular.module('starter')
 .controller('AnnunciUtenteCtrl', function($scope, $http, $timeout, $localStorage, $stateParams) {
 
   var link = "http://mercalinks.altervista.org/select1.php";
-  //{"status":"OK","utente":{"0":"6","1":"utente12@gmail.com","2":"12","3":"Dodici",
-  //"4":"DodiciCognome","5":"121212121212","id_utente":"6","email":"utente12@gmail.com",
-  //"pass":"12","nome":"Dodici","cognome":"DodiciCognome","num_tel":"121212121212"}}
   var idU=$localStorage.id_utente["utente"]["id_utente"];
   $scope.nomeU=$localStorage.id_utente["utente"]["nome"];
   console.log(idU + $scope.nomeU);
   $scope.annunciU = [];
 
-  /*
-  idU=1;
-  //*/
   //filtro gli annunci tenendomi quelli dell'utente
   $http.get(link, {
     params: {
