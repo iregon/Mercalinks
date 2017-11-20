2a8e2bc504c851c02ec6838760826600d997c3f1
spriculo <francescorocca9@gmail.com>
2017-11-20 11:35:31 +0100

    Inserimento annuncio

--- www/js/app.js
+++ www/js/app.js
@@ -36,8 +36,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngStorage'])
     url: '/inserisciAnnuncio',
     views: {
       'menuContent': {
-        templateUrl: 'templates/InserisciAnnuncio/InserisciAnnuncio.html',
-        controller: 'InserisciAnnuncioCtrl'
+        templateUrl: 'templates/inserisciAnnuncio/inserisciAnnuncio.html',
+        controller: 'inserisciAnnuncioCtrl'
       }
     }
   })
@@ -79,7 +79,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngStorage'])
         }
       }
     })
-    
+
     .state('app.annuncio', {
       url: '/annuncio/:annuncioId',
       views: {
--- www/templates/InserisciAnnuncio/controller.js
+++ www/templates/InserisciAnnuncio/controller.js
@@ -1,24 +1,24 @@
-// angular.module('starter')
-//
-// .controller('InserisciAnnuncioCtrl',function($scope,$http){
-//
-//   $scope.inserisci=function(){
-//     var link= "http://mercalinks.altervista.org/insert1.php";
-//     $http.get(link, {
-//       params: {
-//         tabella:"annunci",
-//         titolo:$scope.titolo,
-//         descrizione:$scope.descrizione,
-//         data_in:$scope.data_in,
-//         prezzo:$scope.prezzo,
-//         id_categoria:$scope.id_categoria,
-//         id_posizione:$scope.id_posizione,
-//         id_comune:$scope.id_comune,
-//         id_utente:$scope.id_utente
-//
-//       }
-//     }).then(function(response){
-//
-//     });
-//   };
-// });
+ angular.module('starter')
+
+ .controller('inserisciAnnuncioCtrl',function($scope,$http){
+
+   $scope.inserisci=function(){
+     var link= "http://mercalinks.altervista.org/insert1.php";
+     $http.get(link, {
+       params: {
+         tabella:"annunci",
+         titolo:$scope.titolo,
+         descrizione:$scope.descrizione,
+         data_in:$scope.data_in,
+         prezzo:$scope.prezzo,
+         id_categoria:$scope.id_categoria,
+         id_posizione:$scope.id_posizione,
+         id_comune:$scope.id_comune,
+         id_utente:$scope.id_utente
+
+       }
+     }).then(function(response){
+       
+     });
+   };
+ });
