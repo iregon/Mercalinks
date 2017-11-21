856211021b37589b4c4beafa28bcba5ecbe32104
SSolid12 <gabrielemilano1999@gmail.com>
2017-11-21 12:08:25 +0100

    RobbaVaria
    Sistemazione distanze delle liste in generale e implementazione grezza del refresh

--- www/templates/home/controller.js
+++ www/templates/home/controller.js
@@ -1,16 +1,36 @@
 angular.module('starter')
 
 .controller('HomeCtrl', function($scope, $http){
+ // $scope.chiamataHttp(function(){
+   var link = "http://mercalinks.altervista.org/select1.php";
+   $http.get(link, {
+     params: {
+       tabella: "annunci"
+     }
+   }).then(function(response) {
+     $scope.annunci = response.data.annunci;
+     console.log($scope.annunci);
+   }).catch(function(error) {
+     console.log(error);
+   });
+ // })
 
-  var link = "http://mercalinks.altervista.org/select1.php";
-  $http.get(link, {
-    params: {
-      tabella: "annunci"
+    $scope.doRefresh= function(){
+      $http.get(link, {
+        params: {
+          tabella: "annunci"
+        }
+      }).then(function(response) {
+        $scope.annunci = response.data.annunci;
+        console.log($scope.annunci);
+      }).catch(function(error) {
+        console.log(error);
+      }).finally(function() {
+       // Stop the ion-refresher from spinning
+       $scope.$broadcast('scroll.refreshComplete');
+     });
     }
-  }).then(function(response) {
-    $scope.annunci = response.data.annunci;
-    console.log($scope.annunci);
-  }).catch(function(error) {
-    console.log(error);
-  });
+
+
+
 });
