bec2deab0ec641e0fad08b8c8b2d8e1390319ac0
iregon <0iregon0@gmail.com>
2017-11-22 12:55:31 +0100

    bugfix

--- www/templates/InserisciAnnuncio/controller.js
+++ www/templates/InserisciAnnuncio/controller.js
@@ -112,12 +112,12 @@
           url: link,
           data: data
       }).then(function (resp) {
-          console.log('Success ' + resp.config.data.immagine.name + 'uploaded. Response: ' + resp.data);
+          // console.log('Success ' + resp.config.data.immagine.name + 'uploaded. Response: ' + resp.data);
       }, function (resp) {
           console.log('Error status: ' + resp.status);
       }, function (evt) {
           var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-          console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+          // console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
       });
       //
       //  $http.post(link, {data}).then(function (res){
