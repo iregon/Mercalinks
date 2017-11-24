1b1f16ac8a6473e52c1a1c72de267197b0b80b04
iregon <0iregon0@gmail.com>
2017-11-24 09:58:34 +0100

    preferiti

--- www/templates/preferiti/controller.js
+++ www/templates/preferiti/controller.js
@@ -1,19 +1,27 @@
 angular.module('starter')
 
-.controller('PreferitiCtrl', function($scope, $http){
- // $scope.chiamataHttp(function(){
+.controller('PreferitiCtrl', function($scope, $http, $localStorage){
    var link = "http://mercalinks.altervista.org/select1.php";
    $http.get(link, {
      params: {
        tabella: "annunci"
      }
    }).then(function(response) {
-     $scope.annunci = response.data.annunci;
-     //console.log($scope.annunci);
+     var annunci = response.data.annunci;
+     var annunci2 = [];
+    var pref = $localStorage.preferiti;
+     annunci.forEach(function(element) {
+        pref.forEach(function(preferito) {
+          if(element["id_annuncio"] == preferito) {
+            annunci2.push(element);
+          }
+        });
+    });
+    $scope.annunci = annunci2;
+
    }).catch(function(error) {
      console.log(error);
    });
- // })
 
     $scope.doRefresh= function(){
       $http.get(link, {
@@ -22,7 +30,6 @@ angular.module('starter')
         }
       }).then(function(response) {
         $scope.annunci = response.data.annunci;
-        //console.log($scope.annunci);
       }).catch(function(error) {
         console.log(error);
       }).finally(function() {
